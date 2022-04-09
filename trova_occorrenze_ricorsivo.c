143f95a3a8831946856bb7a72ce03a45c8f5806d
Zeo <leonardo.basso02@gmail.com>
2022-04-09 12:36:48 +0200

    esercizio sulla ricerca ricorsiva di un elemento finito

---
+++ trova_occorrenze_ricorsivo.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int trova_occorrenze(int *X, int dim, int x, int n); 
+int trova_occorrenze_2(int *X, int dim, int x);
+
+int main() {
+   int numeri[8]; 
+   int n;
+   scanf("%d", &n);
+   for (int i = 0; i < 8; i++) {
+       scanf("%d", &numeri[i]);
+   }
+   int n_occorrenze = trova_occorrenze_2(numeri, 8, n);
+   printf("il numero di occorrenze di 2: %d", n_occorrenze);
+}
+
+int trova_occorrenze(int *X, int dim, int x, int n) {
+    /* PRE: X è un array conenente dim elementi
+     * POST: il numero n contiene il numero di occorenze di x i 
+     */
+    if (dim == 0) // CASO BASE: ritorna n = 0; questo in quanto non vi sono elementi nell'array (dim == 0).
+        return n; 
+    /* CASO RICORSIVO:
+     * X[0] == x si richiama la funzione, la cui POST supponiamo essere vera. 
+     * in questo caso n = n + 1
+     * X[0] != x richiamiammo la funzione, la cui POST supponiamo vera.
+     * decrementiamo la dimensione dell'array nel quale cercare
+     * così facendo al passo induttivo, supponendo la POST vera, si aumenta o meno il valore
+     * di n al passo X[0], X[1]....X[dim -1]
+     */
+    if (*X == x)  
+        return trova_occorrenze(X + 1, dim - 1, x, n+=1);
+    else
+        return trova_occorrenze(X + 1, dim - 1, x, n); 
+
+}
+
+int trova_occorrenze_2(int X[], int dim, int x) {
+    /* PRE: X è un array conenente dim elementi
+     * POST: il numero n contiene il numero di occorenze di x i 
+     */
+    if (dim == 0) // CASO BASE: 
+       return 0;
+    if (X[0] == x)
+       return 1 + trova_occorrenze_2(X + 1, dim - 1, x);
+    else
+       return trova_occorrenze_2(X + 1, dim - 1, x);
+    
+}
