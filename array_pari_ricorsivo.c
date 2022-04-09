7c3b7e5c25a21aaebda323cbf9beb1992cf03737
Zeo <leonardo.basso02@gmail.com>
2022-04-09 16:54:03 +0200

    corretto esercizio array pari definitivamente

--- array_pari_ricorsivo.c
+++ array_pari_ricorsivo.c
@@ -2,13 +2,14 @@
 
 int array_pari(int *p, int dim) {
     if (dim == 0)
-        return 0; 
+        return 1; // caso base 
+    /* caso ricorsivo */
     if (*p%2 != 0) // caso in cui l'elemento sia dispari
         return 0;
-    return array_pari(p + 1);
+    return array_pari(p + 1, dim - 1);
 }
 int main() {
-    int numeri[] = {2, 3, 4, 6};
+    int numeri[] = {2, 4, 4, 6};
     int pari = array_pari(numeri, sizeof(numeri) / sizeof(int));
     if (pari)
         printf("l'array è pari");
