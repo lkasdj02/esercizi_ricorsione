cd0703fe2c588c585efdc9ae3cda709da5b2d154
Zeo <leonardo.basso02@gmail.com>
2022-06-29 17:25:27 +0200

    aggiunto esercizio sul massimo comune divisore tra due numeri in input

---
+++ MCD_rec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int mcd(int n1, int n2, int n) {
+    if (n1 % n == 0 && n2 % n == 0) { // CASO BASE
+            return n;
+    }
+    return mcd(n1, n2, n - 1);
+}
+
+int main() {
+    int n1;
+    int n2;
+    printf("inserire il primo numero: "); 
+    scanf("%d", &n1);
+    printf("inserire il secondo numero: "); 
+    scanf("%d", &n2);
+    int minore = n1 < n2 ? n1 : n2;
+
+    printf("\n"); 
+    
+    int MCD = mcd(n1, n2, minore);
+    printf("il massimo comune divisore tra n1 ed n2 è: %d\n", MCD); 
+}
+
